d8dba9b267fa2c371f7469cc28e71e3b2c118fa1
diman <diman@example.com>
2015-04-13 22:56:53 +0600

    подправил

--- tasks/sql/index.php
+++ tasks/sql/index.php
@@ -6,11 +6,11 @@ define("sqlUser", "root");
 define("sqlPass", "vertrigo");
 define("sqlDbName", "sqli");
 $sqlUsers = "hamsters";
-$sqlTrans = "transactions";
+$sqlTrans = "transact";
 
 function connect_db() {
-	$sqlID = mysql_connect(sqlHost, sqlUser, sqlPass) or die('error 1: '.mysql_error());
-	mysql_select_db(sqlDbName, $sqlID) or die('error 2: '.mysql_error());
+	$sqlID = mysql_connect(sqlHost, sqlUser, sqlPass);
+	mysql_select_db(sqlDbName, $sqlID);
 }
 
 if (!session_id()) session_start();
@@ -21,41 +21,16 @@ if (!isset($_SESSION["user"])) {
 }
 
 connect_db();
-if (isset($_POST["payment"])) {
-	$money = min(10005000, max((int) $_POST["money"], 0));
-	if ($money > 0) {
-		$query = "INSERT INTO `{$sqlTrans}` (`user`, `value`, `percent`, `date`) VALUES ".
-			"('{$_SESSION["user"]}', {$money}, 0.0, NOW())";
-		mysql_query($query) or die(mysql_error());
-	}
-}
-if (isset($_POST["away"])) {
-	die("Приносим извинения");
-}
-
-$query = "SELECT SUM(value) as balance FROM `transactions` WHERE `user`='{$_SESSION['user']}'";
+$query = "SELECT SUM(value) as balance FROM `{$sqlTrans}` WHERE `user`='{$_SESSION['user']}'";
 $result = mysql_query($query);
 $balance = mysql_fetch_object($result)->balance;
-/*
-if (isset($_POST["filter"])) {
-	$message = $_POST["filter"];
-} else {
-	#$message = "') LIMIT 0,0 UNION SELECT ".
-	#	"if(ascii(substring((SELECT column_name from information_schema.columns where table_schema=database() and table_name like 'bank%' limit 0,1),1,1))<=110, sleep(5), null)-- ";
-	$message = "') LIMIT 0,0 UNION SELECT ".
-		"substring((SELECT `password` from `bank_users` where `login`='admin' limit 0,1),1,5)-- ";
-	$message = "пасиб";
-}
-*/
 
 function sanitize_date($data) {
-	return "STR_TO_DATE('{$data}', '%Y-%m-%d %H:%i')";
+	//return "STR_TO_DATE('{$data}', '%Y-%m-%d %H:%i')";
+	return "'".$data."'";
 }
 $between = "";
-$_POST = array('history' => 1, 'from' => '', 'to' => '');
 if (isset($_POST["history"])) {
-	$_POST["to"] = "', '') UNION SELECT ".
-		"if(ascii(substring((SELECT table_name from information_schema.tables where table_schema=database() limit 0,1),1,1))<110, sleep(5), null)-- ";
 	$from = sanitize_date($_POST["from"]);
 	$to = sanitize_date($_POST["to"]);
 	$between = " AND `date` BETWEEN {$from} AND {$to}";
@@ -63,20 +38,16 @@ if (isset($_POST["history"])) {
 
 $filter = " WHERE `user`='{$_SESSION["user"]}'".$between;
 $query = "SELECT SUM(value) as yield FROM `{$sqlTrans}`{$filter}";
-var_dump($query);
-$result = mysql_query($query) or die(mysql_error());
-var_dump(mysql_num_rows($result));
-$row = mysql_fetch_assoc($result);
-var_dump($row);
-$yield = (int) $row["yield"];
+$result = mysql_query($query) or die();
+//var_dump(mysql_num_rows($result));
+$yield = (int) mysql_fetch_object($result)->yield;
 
-$transactions = array();
-$query = "SELECT * FROM `{$sqlTrans}`{$filter}";
-var_dump($query);
-$result = mysql_query($query) or die(mysql_error());
-var_dump(mysql_num_rows($result));
+$trans = array();
+$query = "SELECT * FROM `{$sqlTrans}`{$filter} ORDER BY `date` DESC";
+$result = mysql_query($query) or die();
+//var_dump(mysql_num_rows($result));
 while ($row = mysql_fetch_assoc($result)) {
-	$transactions[] = $row;
+	$trans[] = $row;
 }
 ?><!DOCTYPE html>
 <html>
@@ -95,16 +66,8 @@ while ($row = mysql_fetch_assoc($result)) {
 	Ваш баланс: <?php echo $balance; ?> руб<br>
 </div>
 <div class="x">
-	<form method="POST">
-	Пополнить на <input type="text" name="money"> руб 
-	<input type="submit" name="payment" value="Пополнить">
-	</form>
-</div>
-<div class="x">
-	<form method="POST">
-	Вывести <input type="text" name="money"> руб 
-	<input type="submit" name="away" value="Вывести">
-	</form>
+	<input type="submit" onclick="alert('Для пополнения баланса отправьте сумму на bitcoin address 15UYRmpTtZCsYufi4SpuVJbGkusygdvls2')" value="Пополнить баланс">
+	<input type="submit" onclick="alert('Уважаемые клиенты!\nСообщаем, что вывод средств временно не работает.\nПочиним в кратчайшие сроки, простите, извините')" value="Вывести">
 </div>
 <div class="x">
 	<form method="POST">
@@ -113,27 +76,29 @@ while ($row = mysql_fetch_assoc($result)) {
 	<input type="text" id="range_to" name="to">
 	<script type="text/javascript">
 		$(function(){
-			$('#range_from').appendDtpicker({
-				"closeOnSelected": true
-			});
-			$('#range_to').appendDtpicker({
-				"closeOnSelected": true
-			});
+			$('#range_from').appendDtpicker({"closeOnSelected": true});
+			$('#range_to').appendDtpicker({"closeOnSelected": true});
 		});
 	</script>
 	<input type="submit" name="history" value="Показать">
 	</form>
 </div>
-<div>
-	Доходность счета: <?php echo $yield; ?> руб
+<div class="x">
+	Доходность счета за период: <?php echo $yield; ?> руб
 </div>
-<div>
-	История транзакций
+<div class="x">
+	История транзакций:
+	<table border="1">
+	<tr><th>№</th><th>Операция</th><th>Сумма</th><th>Процент</th><th>Дата</th></tr>
 	<?php
-		foreach ($transactions as $row) {
-			var_dump($row);
+		function td($arr) {
+			return "<td>".implode("</td><td>", $arr)."</td>";
+		}
+		for ($i = 0; $i < count($trans); ++$i) {
+			echo "<tr>".td(array($i+1, $trans[$i]["type"], $trans[$i]["value"], $trans[$i]["percent"], $trans[$i]["date"]))."</tr>\r\n";
 		}
 	?>
+	</table>
 </div>
 </body>
 </html>
--- tasks/sql/sqli.sql
+++ tasks/sql/sqli.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 4.3.13
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Апр 11 2015 г., 22:27
--- Версия сервера: 5.5.34
--- Версия PHP: 5.3.27
+-- Время создания: Апр 13 2015 г., 17:54
+-- Версия сервера: 5.6.23
+-- Версия PHP: 5.4.39
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,44 +27,73 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `hamsters` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user` varchar(256) NOT NULL,
-  `pass` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `login` (`user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `pass` varchar(256) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Дамп данных таблицы `hamsters`
 --
 
 INSERT INTO `hamsters` (`id`, `user`, `pass`) VALUES
-(1, 'admin', '8b55613d6611575b6bfe705e54d2cf2b');
+(1, 'admin', '8b55613d6611575b6bfe705e54d2cf2b'),
+(2, 'u5296', '68c5787297cba0baf5e96f1eeb6cb1a7');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `transactions`
+-- Структура таблицы `transact`
 --
 
-CREATE TABLE IF NOT EXISTS `transactions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `transact` (
+  `id` int(11) NOT NULL,
   `user` varchar(256) NOT NULL,
+  `type` varchar(256) NOT NULL,
   `value` int(10) unsigned NOT NULL,
   `percent` float NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `date` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Дамп данных таблицы `transactions`
+-- Дамп данных таблицы `transact`
 --
 
-INSERT INTO `transactions` (`id`, `user`, `value`, `percent`, `date`) VALUES
-(1, 'admin', 1337, 0, '2015-04-12 02:47:58'),
-(3, 'admin', 2, 0, '2015-04-12 03:26:31'),
-(4, 'admin', 0, 0, '2015-04-12 03:26:41');
+INSERT INTO `transact` (`id`, `user`, `type`, `value`, `percent`, `date`) VALUES
+(1, 'u5296', 'Пополнение', 1337, 0, '2015-04-13 16:22:43'),
+(2, 'u5296', 'Прибыль', 40, 3, '2015-04-13 16:23:17'),
+(3, 'u5296', 'Прибыль', 68, 5, '2015-04-13 16:25:28');
 
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `hamsters`
+--
+ALTER TABLE `hamsters`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `login` (`user`);
+
+--
+-- Индексы таблицы `transact`
+--
+ALTER TABLE `transact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `hamsters`
+--
+ALTER TABLE `hamsters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `transact`
+--
+ALTER TABLE `transact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
